1f963fc8f414d6da27bd9bf59111c1ea4be782d0
Aditya Fakhri <adityafakhri04@gmail.com>
2026-08-28 21:57:26 +0700

    chore(branding): update favicons and app title to Dodolan Store
    Replace old SVG and ICO favicon assets with new logo-only PNG files, update the site template to use the new favicon links, and set the default app title from Laravel to Dodolan Store.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,16 +10,16 @@
             }
         </style>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" href="/assets/logo/logo-only.png">
+        <link rel="shortcut icon" href="/assets/logo/logo-only.png">
+        <link rel="apple-touch-icon" href="/assets/logo/logo-only.png">
 
         @fonts
 
         @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
         <x-inertia::head>
-            <title>{{ config('app.name', 'Laravel') }}</title>
+            <title>{{ config('app.name', 'Dodolan Store') }}</title>
         </x-inertia::head>
     </head>
     <body class="font-sans antialiased bg-white text-slate-900">
